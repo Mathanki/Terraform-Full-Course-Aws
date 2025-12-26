d671a2b0f32405e256654db34fc437502705dd80
mathanki <mathanki091suren@gmail.com>
2025-12-25 23:02:25 -0500

    Day24 changes

---
+++ Day24/asg.tf
@@ -0,0 +1,97 @@
+
+
+# Create the IAM Role for the EC2
+resource "aws_iam_role" "ssm_role" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+# Attach the SSM Managed Policy
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Create the Instance Profile to use in your Launch Template
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ec2-ssm-instance-profile"
+  role = aws_iam_role.ssm_role.name
+}
+
+resource "aws_launch_template" "app" {
+  name_prefix   = "app-launch-template-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [ aws_security_group.app_sg.id]
+  }
+
+ #Attaching the IAM Profile for Systems Manager (SSM) access
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ssm_profile.name
+  }
+
+  user_data = filebase64("${path.module}/scripts/user_data.sh")
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  monitoring {
+    enabled = true
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "app-instance"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "app_asg" {
+  name                      = "app-asg"
+  min_size                  = var.min_size
+  max_size                  = var.max_size
+  desired_capacity          = var.desired_capacity
+  vpc_zone_identifier       = aws_subnet.private[*].id
+  target_group_arns         = [aws_lb_target_group.app_tg.arn]
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "app-instance"
+    propagate_at_launch = true
+  }
+}
+# Target Tracking Scaling Policy - CPU Utilization
+resource "aws_autoscaling_policy" "target_tracking" {
+  name                   = "target-tracking-policy"
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 70.0
+  }
+}
